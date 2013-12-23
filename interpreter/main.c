d33bad4b61a8dca1fbeb921b761fd6293001652f
Lecoconnier David <david.lecoconnier@free.fr>
2013-12-23 23:18:23 +0100

    Identation

--- interpreter/main.c
+++ interpreter/main.c
@@ -2,30 +2,34 @@
 #include <stdlib.h>
 #include "interpreter.h"
 
+int main(int argc, char*argv[])
+{
+    Environment *topEnv = createTopFrame();
 
+    if (argc > 1)
+    {
+        int i;
 
-int main(int argc, char*argv[]) {
-  Environment *topEnv = createTopFrame();
+        for (i = 1; i < argc ; i++)
+        {
+            FILE *file = fopen( argv[i], "r" );
 
-  if (argc > 1) {
-    int i;
-
-    for (i = 1; i < argc ; i++) {
-      FILE *file = fopen( argv[i], "r" );
-
-        /* fopen returns 0, the NULL pointer, on failure */
-        if (!file) {
-	  printf( "Could not load: %s\n", argv[i]);
-	  return 1;
-        } else {
-	  printf("loading... %s \n", argv[i]);
-	  loadFromFile(file, topEnv);
+            /* fopen returns 0, the NULL pointer, on failure */
+            if (!file)
+            {
+                printf( "Could not load: %s\n", argv[i]);
+                return 1;
+            }
+            else
+            {
+                printf("loading... %s \n", argv[i]);
+                loadFromFile(file, topEnv);
+            }
         }
     }
-  }
+    interface(topEnv);
 
-  interface(topEnv);
-  
-  destroyTopFrame(topEnv);
-  return -1;
+    destroyTopFrame(topEnv);
+    return -1;
 }
+
--- interpreter/value.h
+++ interpreter/value.h
@@ -1,6 +1,6 @@
-/* By Jie Lin, Kan Wang, Daniel Alabi, Yichen Shen.
-   CS 251, Fall 2011
-*/
+/** By Jie Lin, Kan Wang, Daniel Alabi, Yichen Shen.
+ *  CS 251, Fall 2011
+ */
 
 enum VALUE_TYPE{
   nullType, tableType, cellType, errorType, booleanType, integerType, floatType, stringType, symbolType, openType, closeType, closureType, envType, primitiveType
@@ -40,60 +40,60 @@ typedef struct __LinkedList{
   struct __Value *head;
 } List;
 
-// This function initializes a linked list. It will assign head as NULL.
+//! This function initializes a linked list. It will assign head as NULL.
 List* initializeList();
 
-// This function inserts a new cons cell to the linked list.
+//! This function inserts a new cons cell to the linked list.
 int insertCell(List *list, Value *value);
 
-// pushes value in the front of the list
+//! pushes value in the front of the list
 int push(List *list, Value *value);
 
-// This function pops the head of the linked list.
+//! This function pops the head of the linked list.
 Value* pop(List *list);
 
-// This function reverses the linked list.
+//! This function reverses the linked list.
 int reverse(List *list);
 
-// deepcopies the linked list into a new one and returns it
+//! deepcopies the linked list into a new one and returns it
 List* deepCopyLinkedList(List* list);
 
-// This function frees cons cells of the linked list.
+//! This function frees cons cells of the linked list.
 void cleanup(Value* head);
 
-// This function prints the each token.
+//! This function prints the each token.
 void printToken(Value* value);
 
-// This function prints the linked list.
+//! This function prints the linked list.
 void printTokens(Value* value);
 
-// This function frees its cons cells and also frees the list.
+//! This function frees its cons cells and also frees the list.
 void destroy(List* list);
 
 
-// destroy the environment and everything in it
+//! destroy the environment and everything in it
 void destroyEnvironment(Environment *env);
 
 
 typedef struct __Closure{
   struct __Value* body;
-  struct __LinkedList* args; 
+  struct __LinkedList* args;
   struct __Environment* parent;
-  char* identifier; 
+  char* identifier;
 }Closure;
 
 
-// initialize a closure by mallocating memory for body, args.
-// args is a "linked list" so this function also mallocates hash table.
+//! initialize a closure by mallocating memory for body, args.
+//! args is a "linked list" so this function also mallocates hash table.
 Closure *initializeClosure(Environment* env);
 
-// cleanup frees the contents of the closure, but does not free closure itself.
+//! cleanup frees the contents of the closure, but does not free closure itself.
 void cleanupClosure(Closure *closure);
 
-// destroy frees the contents of the closure and closure itself.
+//! destroy frees the contents of the closure and closure itself.
 void destroyClosure(Closure *closure);
 
-// gives an identifier to a closure
+//! gives an identifier to a closure
 void nameClosure(Closure *closure, char *id);
 
 typedef struct __HashTable{
@@ -102,123 +102,123 @@ typedef struct __HashTable{
   int capacity; //the maximum size
 }HashTable;
 
-// The following are functions for hash table.
-// Note, every parameter except atmoic type (int, float, one character) must be mallocated in the heap.
-/* 
-   This function initializes one hash table with size = capacity
-   If capacity is less than or equal to zero, NULL is returned.
-*/
+//! The following are functions for hash table.
+//! Note, every parameter except atmoic type (int, float, one character) must be mallocated in the heap.
+/**
+ *  This function initializes one hash table with size = capacity
+ *  If capacity is less than or equal to zero, NULL is returned.
+ */
 HashTable* initializeTable(int size);
 
-/*
-  Hash function that uses open addressing.
-  No duplicate keys are allowed.
-*/
+/**
+ * Hash function that uses open addressing.
+ * No duplicate keys are allowed.
+ */
 int hash(HashTable* table, char* id);
 
-/*
-  Insert key = id, payload = value into the hash table.
-  Auto doubling the hash table if the size of the hash table reaches 2/3 of the capacity, 
-  where 2/3 is the load factor.
-*/
+/**
+ * Insert key = id, payload = value into the hash table.
+ * Auto doubling the hash table if the size of the hash table reaches 2/3 of the capacity,
+ * where 2/3 is the load factor.
+ */
 int insertItem(HashTable* table, char* id, Value* value);
 
-/*
-  Auto doubling the hash table if the size of the hash table reaches 2/3 of the capacity, 
-  where 2/3 is the load factor.
-*/
+/**
+ * Auto doubling the hash table if the size of the hash table reaches 2/3 of the capacity,
+ * where 2/3 is the load factor.
+ */
 int autoDouble(HashTable* table);
 
-/*
-  Return the entry with key = id
-*/
+/**
+ * Return the entry with key = id
+ */
 ConsCell* lookupEntry(HashTable* table, char* id);
 
 
-/*
-  Return the payload with key = id
-*/
+/**
+ * Return the payload with key = id
+ */
 Value* lookup(HashTable* table, char* id);
 
-/*
-  Delete an item with key = id.
-  This function does not free the payload since we did not malloc memory 
-  inside the insertItem function for payload.
-*/
+/**
+ * Delete an item with key = id.
+ * This function does not free the payload since we did not malloc memory
+ * inside the insertItem function for payload.
+ */
 int deleteItem(HashTable* table, char* id);
 
-// cleanupTable only cleans the cons cells and keys.
+//! cleanupTable only cleans the cons cells and keys.
 void cleanupTable(HashTable* table);
 
 void cleanupEnvironment(Environment *env);
-/* 
-   Destroy the table by freeing the cons cells, keys and hash table itself.
-   This function cannot free any payloads.
-*/
+/**
+ * Destroy the table by freeing the cons cells, keys and hash table itself.
+ * This function cannot free any payloads.
+ */
 void destroyTable(HashTable *table);
 
-/*
-  Print the table for debugging purposes.
-*/
+/**
+ * Print the table for debugging purposes.
+ */
 void printTable(HashTable* table);
 
-/*
-  returns the car of the List
-*/
+/**
+ * returns the car of the List
+ */
 Value *car (Value *value, Environment *env);
 
-/*
-  returns the cdr of the List
-*/
+/**
+ * returns the cdr of the List
+ */
 Value *cdr (Value *value, Environment *env);
 
-/*
-  RETURNS THE CONS OF THE LIST
-*/
+/**
+ * RETURNS THE CONS OF THE LIST
+ */
 Value *cons (Value *value, Environment *env);
 
-// clean ups value
+//! clean ups value
 void cleanupValue(Value* value);
 
-/*
-  This function is used to print the result after evaluation.
-*/
+/**
+ * This function is used to print the result after evaluation.
+ */
 void printValue(Value* head);
 
-/*
-give me the length of the list, whether proper or improper
-*/
+/**
+ * give me the length of the list, whether proper or improper
+ */
 int listLength(Value *value);
 
 
-/*
-  This function accepts a Value that is the head of the list, and prints out the list.
-*/
+/**
+ * This function accepts a Value that is the head of the list, and prints out the list.
+ */
 void printList(Value* value);
 
-/*
-Determines the length of a proper list
-*/
+/**
+ * Determines the length of a proper list
+ */
 int properListLength(Value *value);
 
-/*
-Determines the length of a proper list
-*/
+/**
+ * Determines the length of a proper list
+ */
 int improperListLength(Value *value);
 
-/*
-returns the first car of the cons cell of the value.
-*/
+/**
+ * returns the first car of the cons cell of the value.
+ */
 Value *getFirst(Value *value);
 
-/*
- returns the cdr of the cons cell of the value
-*/
+/**
+ * returns the cdr of the cons cell of the value
+ */
 Value *getTail(Value *value);
 
-/*
-generic deepCopy which calls subsequent ones
-*/
+/**
+ * generic deepCopy which calls subsequent ones
+ */
 Value *deepCopy(Value *value);
 
 
@@ -230,9 +230,9 @@ Value *deepCopyEnv(Value * value);
 
 Value *deepCopyTable(Value * value);
 
-/*
-recursively. frees a value
-*/
+/**
+ * recursively. frees a value
+ */
 void freeValue(Value *value);
 
 void destroyTopFrame(Environment *env);
@@ -241,14 +241,14 @@ void destroyFrame(Environment *env);
 
 
 void removeLast(Value* value);
-// finds the last element in the value structure
+//! finds the last element in the value structure
 Value *findLast(Value* value);
-// helper for findlast
+//! helper for findlast
 Value *findLastProperRec(Value *value);
-// find the last cons cell in an improper list
+//! find the last cons cell in an improper list
 Value *findLastImproperRec(Value *value);
 
-// converts an int to string
+//! converts an int to string
 char* intToString(int number);
 
 
@@ -257,11 +257,11 @@ char* getReturnString(int number);
 
 Value *letEnvLookup(Environment * bottom, Environment *top, char *id);
 
-// determines if a list is proper or not
+//! determines if a list is proper or not
 int isProperList(Value *value);
 
-// helper function for isproperlist
+//! helper function for isproperlist
 int isProperListRec(Value *value);
 
-// inserts a value into the environment, parent
+//! inserts a value into the environment, parent
 Value* insertValueToEnv(Value *toInsert,Environment *parent);
